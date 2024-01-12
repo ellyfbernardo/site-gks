75a4c2380d27b79371f859482f696e287458c624
Ellyf Bernardo <ellyfbernardo12@gmail.com>
2024-01-12 20:15:36 -0300

    Configuring folders

---
+++ GKS Kite School/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home(){
+     return(
+          <>
+          
+               <h1>GKS KITE SCHOOL</h1>
+          
+          
+          
+          </>
+     )
+}
+
+export default Home;
